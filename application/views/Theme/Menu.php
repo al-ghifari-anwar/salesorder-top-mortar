39f4f53d57b1ffdfbf592fff01a8d07448ac3f3f
Al Ghifari <alghifari.anwar2002@gmail.com>
2023-09-16 12:52:03 +0700

    Logo:

--- application/views/Theme/Menu.php
+++ application/views/Theme/Menu.php
@@ -75,8 +75,8 @@
         <aside class="main-sidebar sidebar-light-primary elevation-4">
             <!-- Brand Logo -->
             <a href="index3.html" class="brand-link">
-                <!-- <img src="dist/img/AdminLTELogo.png" alt="AdminLTE Logo" class="brand-image img-circle elevation-3" style="opacity: .8"> -->
-                <span class="brand-text font-weight-light">Top Mortar</span>
+                <img src="<?= base_url('assets/img/logo_retina.png') ?>" alt="Logo" class="brand-image" style="opacity: .8">
+                <span class="brand-text font-weight-light">.</span>
             </a>
 
             <!-- Sidebar -->
