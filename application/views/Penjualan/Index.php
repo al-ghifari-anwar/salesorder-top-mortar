d968a328caf9014029603ff89ddaa83d02fe1d4f
Al Ghifari <alghifari.anwar2002@gmail.com>
2023-10-10 00:24:30 +0700

    Change nama produk styleling

--- application/views/Penjualan/Index.php
+++ application/views/Penjualan/Index.php
@@ -42,7 +42,7 @@
                         <!-- small box -->
                         <div class="small-box bg-light">
                             <div class="inner">
-                                <h3><?= str_replace('TOP MORTAR', '', $data['nama_produk']) ?></h3>
+                                <p style="font-size: 20pt;"><b><?= str_replace('TOP MORTAR', '', $data['nama_produk']) ?></b></p>
 
                                 <p><?= $data['qty_produk'] ?> Sak</p>
                             </div>
