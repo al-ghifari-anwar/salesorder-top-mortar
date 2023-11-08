85fa257485e2deaf05cd4602affe197b723bedae
Al Ghifari <alghifari.anwar2002@gmail.com>
2023-11-08 10:44:57 +0700

    Tanggal dan jam visit

--- application/views/Visit/List.php
+++ application/views/Visit/List.php
@@ -50,6 +50,7 @@
                                         <th>Sales</th>
                                         <th>Toko</th>
                                         <th>Jarak</th>
+                                        <th>Jam - Tanggal</th>
                                         <th>Laporan</th>
                                         <th>Aksi</th>
                                     </tr>
@@ -63,6 +64,7 @@
                                             <td><?= $data['full_name'] ?></td>
                                             <td><?= $data['nama'] ?></td>
                                             <td><?= $data['distance_visit'] ?></td>
+                                            <td><?= date("H:i - d M Y", strtotime($data['date_visit'])) ?></td>
                                             <td><?= $data['laporan_visit'] ?></td>
                                             <td>
                                                 <a href="<?= base_url('approve-visit/' . $data['id_visit']) ?>" class="btn btn-success" title="Approve"><i class="fas fa-check-circle"></i></a>
