1b958f596ecf4fc6fd3b86256035a8e020a98cb1
Al Ghifari Anwar <alghifari.anwar2002@gmail.com>
2023-11-25 09:26:47 +0700

    Auto invoice

--- application/controllers/SuratJalan.php
+++ application/controllers/SuratJalan.php
@@ -186,6 +186,28 @@ class SuratJalan extends CI_Controller
 
         if ($status == "success") {
             $this->db->update('tb_surat_jalan', ['is_finished' => 1], ['id_surat_jalan' => $suratjalan['id_surat_jalan']]);
+
+            $curl = curl_init();
+
+            curl_setopt_array($curl, array(
+                CURLOPT_URL => 'https://saleswa.topmortarindonesia.com/invoice.php',
+                CURLOPT_RETURNTRANSFER => true,
+                CURLOPT_ENCODING => '',
+                CURLOPT_MAXREDIRS => 10,
+                CURLOPT_TIMEOUT => 0,
+                CURLOPT_FOLLOWLOCATION => true,
+                CURLOPT_HTTP_VERSION => CURL_HTTP_VERSION_1_1,
+                CURLOPT_CUSTOMREQUEST => 'POST',
+                CURLOPT_POSTFIELDS => array('id_surat_jalan' => "'" . $id . "'"),
+            ));
+
+            $response = curl_exec($curl);
+
+            curl_close($curl);
+
+            // echo $response;
+            // die;
+
             $this->session->set_flashdata('success', "Surat jalan berhasil dibuat!");
             redirect('surat-jalan');
         } else {
--- application/models/MSuratJalan.php
+++ application/models/MSuratJalan.php
@@ -78,24 +78,6 @@ class MSuratJalan extends CI_Model
         $query = $this->db->insert('tb_surat_jalan', $this);
 
         if ($query) {
-            $curl = curl_init();
-
-            curl_setopt_array($curl, array(
-                CURLOPT_URL => 'https://saleswa.topmortarindonesia.com/invoice.php',
-                CURLOPT_RETURNTRANSFER => true,
-                CURLOPT_ENCODING => '',
-                CURLOPT_MAXREDIRS => 10,
-                CURLOPT_TIMEOUT => 0,
-                CURLOPT_FOLLOWLOCATION => true,
-                CURLOPT_HTTP_VERSION => CURL_HTTP_VERSION_1_1,
-                CURLOPT_CUSTOMREQUEST => 'POST',
-                CURLOPT_POSTFIELDS => array('id_surat_jalan' => $this->db->last_query()),
-            ));
-
-            $response = curl_exec($curl);
-
-            curl_close($curl);
-
             redirect('surat-jalan/' . $this->db->insert_id());
         } else {
             $this->session->set_flashdata('failed', "Gagal menyimpan data surat jalan!");
