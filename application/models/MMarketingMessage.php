59b865f684917852b004d904a3f8f0a6ec9a4f85
Al Ghifari Anwar <alghifari.anwar2002@gmail.com>
2024-02-05 10:49:03 +0700

    Error print

--- application/models/MMarketingMessage.php
+++ application/models/MMarketingMessage.php
@@ -28,10 +28,11 @@ class MMarketingMessage extends CI_Model
     {
         $post = $this->input->post();
         $upload = $this->uploadImage($post['nama_marketing_message']);
-        if ($upload != false) {
+        if ($upload['status'] == 'success') {
+            $data = $upload['message'];
             $this->nama_marketing_message = $post['nama_marketing_message'];
             $this->template_id = $post['template_id'];
-            $this->image_marketing_message = $upload['file_name'];
+            $this->image_marketing_message = $data['file_name'];
             $this->body_marketing_message = $post['body_marketing_message'];
             $this->week_marketing_message = $post['week_marketing_message'];
             $this->target_status = $post['target_status'];
@@ -45,33 +46,34 @@ class MMarketingMessage extends CI_Model
                 return false;
             }
         } else {
-            return false;
+            $this->session->set_flashdata('failed', "Error: " . $upload['message']);
+            redirect('marketing');
         }
     }
 
-    public function update($id)
-    {
-        $post = $this->input->post();
+    // public function update($id)
+    // {
+    //     $post = $this->input->post();
 
-        $upload = $this->uploadImage($post['nama_marketing_message']);
-        if ($upload != false) {
-            $this->nama_marketing_message = $post['nama_marketing_message'];
-            $this->template_id = $post['template_id'];
-            $this->image_marketing_message = $upload['file_name'];
-            $this->body_marketing_message = $post['body_marketing_message'];
-            $this->week_marketing_message = $post['week_marketing_message'];
-            $this->target_status = $post['target_status'];
-            $this->id_distributor = $this->session->userdata('id_distributor');
+    //     $upload = $this->uploadImage($post['nama_marketing_message']);
+    //     if ($upload['status'] == 'success') {
+    //         $this->nama_marketing_message = $post['nama_marketing_message'];
+    //         $this->template_id = $post['template_id'];
+    //         $this->image_marketing_message = $upload['file_name'];
+    //         $this->body_marketing_message = $post['body_marketing_message'];
+    //         $this->week_marketing_message = $post['week_marketing_message'];
+    //         $this->target_status = $post['target_status'];
+    //         $this->id_distributor = $this->session->userdata('id_distributor');
 
-            $query = $this->db->update('tb_marketing_message', $this, ['id_marketing_message' => $id]);
+    //         $query = $this->db->update('tb_marketing_message', $this, ['id_marketing_message' => $id]);
 
-            if ($query) {
-                return true;
-            } else {
-                return false;
-            }
-        }
-    }
+    //         if ($query) {
+    //             return true;
+    //         } else {
+    //             return false;
+    //         }
+    //     }
+    // }
 
     public function delete($id)
     {
@@ -91,18 +93,18 @@ class MMarketingMessage extends CI_Model
         $config['allowed_types']        = 'gif|jpg|jpeg|png';
         $config['file_name']            = $file_name;
         $config['overwrite']            = true;
-        $config['max_size']             = 2048; //2MB
+        $config['max_size']             = 5000; //5MB
 
         $this->load->library('upload', $config);
 
         if (!$this->upload->do_upload('image_marketing_message')) {
             $data['error'] = $this->upload->display_errors();
 
-            return false;
+            return ['status' => 'error', 'message' => $this->upload->display_errors()];
         } else {
             $uploaded_data = $this->upload->data();
 
-            return $uploaded_data;
+            return ['status' => 'success', 'message' => $uploaded_data];
         }
     }
 }
