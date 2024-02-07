5e57bf7e17a25faaf3c20fff34c5b4e4447a2a57
Al Ghifari Anwar <alghifari.anwar2002@gmail.com>
2024-02-07 07:48:25 +0700

    chek

--- application/views/JatuhTempo/Print.php
+++ application/views/JatuhTempo/Print.php
@@ -278,7 +278,11 @@ function penyebut($nilai)
                 <?php
                 $id_invoice = $storeInv3['id_invoice'];
                 $payment = $this->db->query("SELECT SUM(amount_payment) AS amount_payment FROM tb_payment WHERE id_invoice = '$id_invoice'")->row_array();
-                $sisaHutang = $storeInv3['total_invoice'] - $payment['amount_payment'];
+                if ($payment) {
+                    $sisaHutang = $storeInv['total_invoice'] - $payment['amount_payment'];
+                } else {
+                    $sisaHutang = $storeInv['total_invoice'];
+                }
                 $totalStore3 += $sisaHutang;
 
                 ?>
