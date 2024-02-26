2630d867c29ac9a5ed7a336ebfa8453fd2443e0a
Al Ghifari Anwar <alghifari.anwar2002@gmail.com>
2024-02-26 17:07:41 +0700

    Fix voucher list

--- application/controllers/Voucher.php
+++ application/controllers/Voucher.php
@@ -78,8 +78,8 @@ class Voucher extends CI_Controller
             $data['voucher'] = $this->db->query("SELECT * FROM tb_voucher JOIN tb_contact ON tb_contact.id_contact = tb_voucher.id_contact WHERE tb_voucher.is_claimed = 1 AND tb_contact.id_city = '$id_city'")->result_array();
             $html = $this->load->view('Voucher/PrintClaimed', $data, true);
         } else if ($berdasarkan == 'not-claimed') {
-            $dateNow = date("Y-m-d 23:59:59");
-            $data['voucher'] = $this->db->query("SELECT * FROM tb_voucher JOIN tb_contact ON tb_contact.id_contact = tb_voucher.id_contact WHERE tb_voucher.is_claimed = 0 AND tb_contact.id_city = '$id_city' AND tb_voucher.exp_date > '$dateNow'")->result_array();
+            $dateNow = date("Y-m-d 00:00:00");
+            $data['voucher'] = $this->db->query("SELECT * FROM tb_voucher JOIN tb_contact ON tb_contact.id_contact = tb_voucher.id_contact WHERE tb_voucher.is_claimed = 0 AND tb_contact.id_city = '$id_city' AND tb_voucher.exp_date >= '$dateNow'")->result_array();
             $html = $this->load->view('Voucher/PrintNotClaimed', $data, true);
         }
         $mpdf->AddPage('L');
