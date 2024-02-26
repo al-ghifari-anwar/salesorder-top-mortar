ca7594910bf18c9c06f0c5c4d82524b13f6787bb
Al Ghifari Anwar <alghifari.anwar2002@gmail.com>
2024-02-26 12:38:19 +0700

    Laporan voucher claim dan not claim

--- application/views/Voucher/PrintNotClaimed.php
+++ application/views/Voucher/PrintNotClaimed.php
@@ -35,7 +35,7 @@ function penyebut($nilai)
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title><?= "Laporan Voucher Sudah Claim " . $city['nama_city']  ?></title>
+    <title><?= "Laporan Voucher Belum Claim " . $city['nama_city']  ?></title>
 </head>
 
 <body>
