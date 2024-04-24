bd0fcf9d780c5f51c168d27772be808e725d5961
Al Ghifari Anwar <alghifari.anwar2002@gmail.com>
2024-04-24 14:26:29 +0700

    fix menu

--- application/views/Theme/Menu.php
+++ application/views/Theme/Menu.php
@@ -358,7 +358,7 @@
                                     </li>
                                 <?php endif; ?>
                             <?php endif; ?>
-                            <?php if ($this->session->userdata('level_user') == 'admin' || $this->session->userdata('level_user') == 'superadmin' || $this->session->userdata('level_user') == 'admin_c') : ?>
+                            <?php if ($this->session->userdata('level_user') == 'admin' || $this->session->userdata('level_user') == 'superadmin' || $this->session->userdata('level_user') == 'admin_c' || $this->session->userdata('level_user') == 'finance') : ?>
                                 <li class="nav-item">
                                     <a href="#" class="nav-link">
                                         <i class="nav-icon fas fa-file"></i>
