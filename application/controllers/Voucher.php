3cb5de8e5600fa7823db2c08240f822cfa03b1b8
Al Ghifari Anwar <alghifari.anwar2002@gmail.com>
2024-06-22 10:10:49 +0700

    piutang timezone

--- application/controllers/Voucher.php
+++ application/controllers/Voucher.php
@@ -243,7 +243,8 @@ class Voucher extends CI_Controller
 
     public function claimed()
     {
-        $wa_token = 'xz5922BoBI6I9ECLKVZjPMm-7-0sqx0cjIqVVeuWURI';
+        // $wa_token = 'xz5922BoBI6I9ECLKVZjPMm-7-0sqx0cjIqVVeuWURI';
+        $wa_token = '_GEJodr1x8u7-nSn4tZK2hNq0M5CARkRp_plNdL2tFw';
         $template_id = '85f17083-255d-4340-af32-5dd22f483960';
         // $integration_id = '31c076d5-ac80-4204-adc9-964c9b0c590b';
 
