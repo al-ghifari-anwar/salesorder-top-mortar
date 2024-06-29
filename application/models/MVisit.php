8a745da77491f55111384ca7970d652d4e739b8f
Al Ghifari Anwar <alghifari.anwar2002@gmail.com>
2024-06-29 18:27:17 +0700

    total renvi

--- application/models/MVisit.php
+++ application/models/MVisit.php
@@ -20,11 +20,12 @@ class MVisit extends CI_Model
     {
         $this->db->join('tb_user', 'tb_user.id_user = tb_visit.id_user');
         $this->db->join('tb_city', 'tb_city.id_city = tb_user.id_city');
+        $this->db->join('tb_contact', 'tb_contact.id_contact = tb_visit.id_contact');
         $this->db->where('MONTH(date_visit)', $month);
         if ($id_user != 0) {
             $this->db->where('tb_visit.id_user', $id_user);
         }
-        $this->db->where('tb_user.id_city', $id_city);
+        $this->db->where('tb_contact.id_city', $id_city);
         $this->db->group_by('tb_user.id_user');
         $query = $this->db->get('tb_visit')->result_array();
         // echo $this->db->last_query();
