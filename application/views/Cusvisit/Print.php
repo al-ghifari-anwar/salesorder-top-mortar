905926e1d4a9fbd540c9f7f4326180d3e83782e6
Al Ghifari Anwar <alghifari.anwar2002@gmail.com>
2024-07-02 07:06:23 +0700

    laporan customer visit

--- application/views/Cusvisit/Print.php
+++ application/views/Cusvisit/Print.php
@@ -126,7 +126,7 @@ function penyebut($nilai)
                         <?php
                         $id_contact = $contact['id_contact'];
                         $this->db->select('COUNT(*) as jmlVisit');
-                        $this->db->group_by('tb_visit.date_visit');
+                        $this->db->group_by('DATE(tb_visit.date_visit)');
                         $getVisit = $this->db->get_where('tb_visit', ['id_contact' => $id_contact, 'DATE(date_visit) >=' => $dateFrom, 'DATE(date_visit) <=' => $dateTo])->row_array();
                         ?>
                         <?= $getVisit == null ? 0 : $getVisit['jmlVisit'] ?>
