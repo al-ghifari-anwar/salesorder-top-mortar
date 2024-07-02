65d62a403b0c11a12836df58724aa5a276a4fd26
Al Ghifari Anwar <alghifari.anwar2002@gmail.com>
2024-07-03 06:46:20 +0700

    laporan penerima

--- application/controllers/Voucher.php
+++ application/controllers/Voucher.php
@@ -58,8 +58,9 @@ class Voucher extends CI_Controller
         // $data['dates'] = explode("-", $dateRange);
         // $this->load->view('Stok/Print', $data);
         $data['dateNow'] = date("Y-m-d");
-        $this->db->join('tb_voucher', 'tb_contact.id_contact = tb_voucher.id_contact', 'LEFT');
-        $data['contacts'] = $this->db->get_where('tb_contact', ['is_claimed' => 0, 'tb_contact.id_city' => $id_city])->result_array();
+        // $this->db->join('tb_voucher', 'tb_contact.id_contact = tb_voucher.id_contact', 'LEFT');
+        // $data['contacts'] = $this->db->get_where('tb_contact', ['is_claimed' => 0, 'tb_contact.id_city' => $id_city])->result_array();
+        $data['contacts'] = $this->db->query("SELECT * FROM tb_contact tc LEFT JOIN tb_voucher tv ON tv.id_contact = tc.id_contact WHERE tc.id_city = $id_city")->result_array();
         // PDF
         $mpdf = new \Mpdf\Mpdf(['format' => 'A4']);
         $mpdf->SetMargins(0, 0, 5);
