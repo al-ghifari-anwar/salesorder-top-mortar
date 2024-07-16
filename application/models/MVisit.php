46ddb96544c9c8946ad0b57759208110628d9537
Al Ghifari Anwar <alghifari.anwar2002@gmail.com>
2024-07-16 14:36:42 +0700

    fix visit

--- application/models/MVisit.php
+++ application/models/MVisit.php
@@ -76,7 +76,7 @@ class MVisit extends CI_Model
         $this->db->join('tb_user', 'tb_user.id_user = tb_visit.id_user');
         $this->db->join('tb_contact', 'tb_contact.id_contact = tb_visit.id_contact');
         $this->db->where_in('tb_user.level_user', ['sales', 'penagihan']);
-        $query = $this->db->get_where('tb_visit', ['tb_user.id_city' => $id_city, 'is_approved' => 0, 'is_deleted' => 0])->result_array();
+        $query = $this->db->get_where('tb_visit', ['tb_contact.id_city' => $id_city, 'is_approved' => 0, 'is_deleted' => 0])->result_array();
 
         return $query;
     }
@@ -111,7 +111,7 @@ class MVisit extends CI_Model
         $this->db->join('tb_contact', 'tb_contact.id_contact = tb_visit.id_contact');
         $this->db->where('MONTH(date_visit) =', $bulan);
         $this->db->group_by('tb_visit.id_contact');
-        $query = $this->db->get_where('tb_visit', ['tb_user.id_city' => $id_city, 'tb_user.id_user' => $id_user, 'is_approved' => 0, 'is_deleted' => 0,])->result_array();
+        $query = $this->db->get_where('tb_visit', ['tb_contact.id_city' => $id_city, 'tb_user.id_user' => $id_user, 'is_approved' => 0, 'is_deleted' => 0,])->result_array();
 
         return $query;
     }
