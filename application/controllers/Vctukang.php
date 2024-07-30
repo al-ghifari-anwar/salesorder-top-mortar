56ae038831b7b9a2fc4ff3aa177671bc1fcd1f7f
Al Ghifari Anwar <alghifari.anwar2002@gmail.com>
2024-07-30 12:01:11 +0700

    Voucher tukang

--- application/controllers/Vctukang.php
+++ application/controllers/Vctukang.php
@@ -55,7 +55,7 @@ class Vctukang extends CI_Controller
                     $config['cacheable']    = true; //boolean, the default is true
                     $config['cachedir']             = './assets/'; //string, the default is application/cache/
                     $config['errorlog']             = './assets/'; //string, the default is application/logs/
-                    $config['imagedir']             = './assets/img/qr-vstukang/'; //direktori penyimpanan qr code
+                    $config['imagedir']             = './assets/img/qr/'; //direktori penyimpanan qr code
                     $config['quality']              = true; //boolean, the default is true
                     $config['size']                 = '1024'; //interger, the default is 1024
                     $config['black']                = array(224, 255, 255); // array, default is array(255,255,255)
@@ -107,7 +107,7 @@ class Vctukang extends CI_Controller
                                         "params": [
                                             {
                                                 "key":"url",
-                                                "value":"https://order.topmortarindonesia.com/img/qr-vctukang/' . $no_seri . '.png"
+                                                "value":"https://order.topmortarindonesia.com/assets/img/qr/' . $no_seri . '.png"
                                             },
                                             {
                                                 "key":"filename",
