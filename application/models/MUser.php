3d86f0c960d614b16bb00526310510eccd045107
Al Ghifari Anwar <alghifari.anwar2002@gmail.com>
2024-08-06 05:32:52 +0700

    Fix claim voucher

--- application/models/MUser.php
+++ application/models/MUser.php
@@ -18,6 +18,12 @@ class MUser extends CI_Model
         return $query;
     }
 
+    public function getAllForManualRenvi($id_city)
+    {
+        $query = $this->db->get_where('tb_user', ['id_city' => $id_city])->result_array();
+        return $query;
+    }
+
     public function getAllDefault()
     {
         $this->db->join('tb_city', 'tb_city.id_city = tb_user.id_city');
