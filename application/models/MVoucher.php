1cbc4bc50146203f17a7c01236e542a892c04baf
Al Ghifari Anwar <alghifari.anwar2002@gmail.com>
2024-08-05 11:01:22 +0700

    Fix claim voucher

--- application/models/MVoucher.php
+++ application/models/MVoucher.php
@@ -84,7 +84,7 @@ class MVoucher extends CI_Model
         $vouchers_ori = "";
         $dateNow = date("Y-m-d H:i:s");
 
-        $query = $this->db->query("SELECT tb_contact.id_contact, SUM(point_voucher) as point_voucher FROM tb_voucher JOIN tb_contact ON tb_contact.id_contact = tb_voucher.id_contact WHERE tb_voucher.is_claimed = 0 AND tb_voucher.no_voucher IN ('" . $no_vouchers . "') ")->row_array();
+        $query = $this->db->query("SELECT tb_contact.id_contact, SUM(point_voucher) as point_voucher FROM tb_voucher JOIN tb_contact ON tb_contact.id_contact = tb_voucher.id_contact WHERE tb_voucher.is_claimed = 0 AND tb_voucher.no_voucher IN ('" . $no_vouchers . "') AND DATE(exp_date) >= DATE(NOW())")->row_array();
 
         $getKode = $this->db->query("SELECT * FROM tb_voucher JOIN tb_contact ON tb_contact.id_contact = tb_voucher.id_contact WHERE tb_voucher.is_claimed = 0 AND tb_voucher.no_voucher IN ('" . $no_vouchers . "') AND DATE(exp_date) >= DATE(NOW())")->result_array();
 
