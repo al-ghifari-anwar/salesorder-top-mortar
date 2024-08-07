70beff15caa574fd5fb037952000c2d39524f9d8
Al Ghifari Anwar <alghifari.anwar2002@gmail.com>
2024-08-07 10:59:32 +0700

    Fix visit manual

--- application/views/Manualvisit/Detail.php
+++ application/views/Manualvisit/Detail.php
@@ -63,7 +63,7 @@
                                         </div>
                                         <div class="form-group">
                                             <label for="">Nominal Pembayaran</label>
-                                            <input type="text" name="pay_value" class="form-control" required>
+                                            <input type="number" name="pay_value" class="form-control" required>
                                             <input type="text" name="is_pay" value="pay" hidden>
                                         </div>
                                     </div>
