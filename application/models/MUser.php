e33524dc5325af4915d3b6173ab33ab954e5e237
Al Ghifari Anwar <alghifari.anwar2002@gmail.com>
2024-08-22 11:20:53 +0700

    absen harian

--- application/models/MUser.php
+++ application/models/MUser.php
@@ -20,7 +20,7 @@ class MUser extends CI_Model
 
     public function getByIdDist($id_distributor)
     {
-        $this->db->where('level_user', 'courier', FALSE);
+        $this->db->where('level_user', 'courier');
         $this->db->or_where('level_user', 'sales', NULL, FALSE);
         $this->db->or_where('level_user', 'penagihan', NULL, FALSE);
         $query = $this->db->get_where('tb_user', ['id_distributor' => $id_distributor, 'password !=' => '0'])->result_array();
