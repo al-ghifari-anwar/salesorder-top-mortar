f0e6b83dc68c6303f343a3579e4854a920dc2544
Al Ghifari Anwar <alghifari.anwar2002@gmail.com>
2024-08-25 03:51:03 +0700

    absen harian

--- application/models/MInvoice.php
+++ application/models/MInvoice.php
@@ -125,7 +125,6 @@ class MInvoice extends CI_Model
         $this->db->join('tb_surat_jalan', 'tb_surat_jalan.id_surat_jalan = tb_invoice.id_surat_jalan');
         $this->db->join('tb_contact', 'tb_contact.id_contact = tb_surat_jalan.id_contact');
         $this->db->join('tb_city', 'tb_contact.id_city = tb_city.id_city');
-        $this->db->join('tb_payment', 'tb_payment.id_invoice = tb_invoice.id_invoice');
         $this->db->order_by('tb_surat_jalan.id_contact', 'ASC');
         $this->db->group_by('tb_invoice.id_invoice');
         if ($id_contact != 0) {
