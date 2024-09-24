86e7f51b0f00f649b1743695fbdcb3235012ad2d
Al Ghifari Anwar <alghifari.anwar2002@gmail.com>
2024-09-24 09:13:28 +0700

    TKP tambah toko via web

--- application/views/Theme/Menu.php
+++ application/views/Theme/Menu.php
@@ -176,7 +176,7 @@
                                 </li>
                             <?php endif; ?>
                             <?php if ($this->session->userdata('level_user') == 'superadmin' || $this->session->userdata('level_user') == 'admin_c' || $this->session->userdata('level_user') == 'admin' || $this->session->userdata('level_user') == 'finance' || $this->session->userdata('level_user') == 'salesleader' || $this->session->userdata('level_user') == 'courier') : ?>
-                                <?php if ($this->session->userdata('level_user') != 'salesleader') : ?>
+                                <?php if ($this->session->userdata('level_user') != 'sales') : ?>
                                     <li class="nav-item">
                                         <a href="#" class="nav-link">
                                             <i class="nav-icon fas fa-database"></i>
