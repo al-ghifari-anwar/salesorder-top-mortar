f78c9a6924a4719ba1ed0c6c6c17901672864d7a
Al Ghifari Anwar <alghifari.anwar2002@gmail.com>
2024-09-28 17:27:12 +0700

    Kota sales top seller

--- application/models/MContact.php
+++ application/models/MContact.php
@@ -50,7 +50,7 @@ class MContact extends CI_Model
     public function getAllTopSeller()
     {
         $this->db->join('tb_city', 'tb_city.id_city = tb_contact.id_city');
-        $query = $this->db->get_where('tb_contact', ['tb_contact.pass_contact !=' => 0])->result_array();
+        $query = $this->db->get_where('tb_contact', ['tb_contact.pass_contact !=' => '0'])->result_array();
         return $query;
     }
 
