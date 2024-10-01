319036f1e65b068c36dac1ee5a2f627014eb1496
Al Ghifari Anwar <alghifari.anwar2002@gmail.com>
2024-10-01 08:35:23 +0700

    Wording optional diganti tidak wajib diisi

--- application/views/Tokopromo/Index.php
+++ application/views/Tokopromo/Index.php
@@ -50,11 +50,11 @@
                                     <input type="text" name="nomorhp" id="" class="form-control" required>
                                 </div>
                                 <div class="form-group">
-                                    <label for="">Tanggal Lahir (Opsional)</label>
+                                    <label for="">Tanggal Lahir (Tidak Wajib Diisi)</label>
                                     <input type="date" name="tgl_lahir" id="" class="form-control">
                                 </div>
                                 <div class="form-group">
-                                    <label for="">Alamat (Opsional)</label>
+                                    <label for="">Alamat (Tidak Wajib Diisi)</label>
                                     <textarea name="address" class="form-control" id="" cols="30" rows="3"></textarea>
                                 </div>
                                 <div class="form-group">
