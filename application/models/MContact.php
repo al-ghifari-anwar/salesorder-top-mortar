d3c0adc9cb14146385d6bdb57e771491b9e31cee
Al Ghifari Anwar <alghifari.anwar2002@gmail.com>
2024-10-02 08:42:30 +0700

    Dist akun seller

--- application/models/MContact.php
+++ application/models/MContact.php
@@ -50,6 +50,7 @@ class MContact extends CI_Model
     public function getAllTopSeller()
     {
         $this->db->join('tb_city', 'tb_city.id_city = tb_contact.id_city');
+        $this->db->where('tb_city.id_distributor', $this->session->userdata('id_distributor'));
         $query = $this->db->get_where('tb_contact', ['tb_contact.pass_contact !=' => '0'])->result_array();
         return $query;
     }
