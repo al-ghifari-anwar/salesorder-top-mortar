91147c3fff016bff7268221b64e813d4cd448a18
Al Ghifari Anwar <alghifari.anwar2002@gmail.com>
2024-12-10 18:25:19 +0700

    renvi mg manual

--- application/views/Akunseller/Penukaran.php
+++ application/views/Akunseller/Penukaran.php
@@ -97,7 +97,7 @@
                                     foreach ($vouchers as $data) : ?>
                                         <?php
                                         $id_contact = $data['id_contact'];
-                                        $this->db->join('tb_city', 'tb_city.id_city = tb_contact.id_city');
+                                        $this->db->join('tb_city', 'tb_city.id_city = tb_contact.id_city', 'LEFT');
                                         $getContact = $this->db->get_where('tb_contact', ['id_contact' => $id_contact])->row_array();
                                         ?>
                                         <tr>
