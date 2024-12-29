2e8e041f70459018dd8e225cedb75c3d09cfa419
Al Ghifari Anwar <alghifari.anwar2002@gmail.com>
2024-12-30 05:55:20 +0700

    rekap piutang

--- application/views/Piutang/Rekap.php
+++ application/views/Piutang/Rekap.php
@@ -43,7 +43,7 @@
                             <h4 class="text-center">Rekap Piutang Per <?= date("d F Y") ?></h4>
                         </div>
                         <div class="card-body">
-                            <table class="table table-bordered" id="example1">
+                            <table class="table table-bordered" id="table">
                                 <thead>
                                     <tr>
                                         <th>No</th>
