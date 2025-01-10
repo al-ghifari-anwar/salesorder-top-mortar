5649e4ed55f29031507a0dee0cd2432f6e4d19cf
Al Ghifari Anwar <alghifari.anwar2002@gmail.com>
2025-01-10 23:00:08 +0700

    demo tokopromo

--- application/controllers/Tokopromo.php
+++ application/controllers/Tokopromo.php
@@ -184,7 +184,7 @@ class Tokopromo extends CI_Controller
 
 
                             // $message = $nama . " tukarkan voucher diskon Rp. 5.000 dengan cara tunjukkan qr ini pada toko. ";
-                            $message = "Halo " . $nama . "*Beli Top Mortar, Kembaliannya bisa buat beli Kopi!* Dapatkan *Potongan Langsung Rp.5,000* setiap pembelian produk Top Mortar di toko bangunan terdekat. Tunjukan QR ini pada toko saat berbelanja \\\\n SK: \\\\n QR hanya berlaku 1x Potongan hanya berlaku per nota belanja Berlaku untuk semua produk top mortar\\\\n Lihat Lokasi Toko: \\\\n https://order.topmortarindonesia.com/penukaranstore  ";
+                            $message = "Halo " . $nama . "*Beli Top Mortar, Kembaliannya bisa buat beli Kopi!* Dapatkan *Potongan Langsung Rp.5,000* setiap pembelian produk Top Mortar di toko bangunan terdekat. Tunjukan QR ini pada toko saat berbelanja SK: QR hanya berlaku 1x Potongan hanya berlaku per nota belanja Berlaku untuk semua produk top mortarLihat Lokasi Toko: https://order.topmortarindonesia.com/penukaranstore  ";
                             // Send message
                             $curl = curl_init();
 
