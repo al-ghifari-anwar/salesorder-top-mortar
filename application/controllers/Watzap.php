4550c574f7fd0a5a0c0766963b5a4e5561b2ccec
Al Ghifari Anwar <alghifari.anwar2002@gmail.com>
2025-02-10 23:32:12 +0700

    Trial Watzap

--- application/controllers/Watzap.php
+++ application/controllers/Watzap.php
@@ -14,54 +14,64 @@ class Watzap extends CI_Controller
         $this->output->set_content_type('application/json');
         $watzapTukang = $this->MWatzaptukang->getSingleWaiting();
 
-        $name = $watzapTukang['name_watzap_tukang'];
-        $phone = $watzapTukang['phone_watzap_tukang'];
+        if ($watzapTukang) {
 
-        $dataSending = array();
-        $dataSending["api_key"] = "OG0UMAWHV6SJ0GL2";
-        $dataSending["number_key"] = "liVWjKEiV6aImQ5M";
-        $dataSending["phone_no"] = $phone;
-        $dataSending["message"] = "Halo " . $name . ", ini adalah trial WA";
+            $name = $watzapTukang['name_watzap_tukang'];
+            $phone = $watzapTukang['phone_watzap_tukang'];
 
-        $curl = curl_init();
-        curl_setopt_array($curl, array(
-            CURLOPT_URL => 'https://api.watzap.id/v1/send_message',
-            CURLOPT_RETURNTRANSFER => true,
-            CURLOPT_ENCODING => '',
-            CURLOPT_MAXREDIRS => 10,
-            CURLOPT_TIMEOUT => 0,
-            CURLOPT_FOLLOWLOCATION => true,
-            CURLOPT_HTTP_VERSION => CURL_HTTP_VERSION_1_1,
-            CURLOPT_CUSTOMREQUEST => 'POST',
-            CURLOPT_POSTFIELDS => json_encode($dataSending),
-            CURLOPT_HTTPHEADER => array(
-                'Content-Type: application/json'
-            ),
-        ));
-        $response = curl_exec($curl);
-        curl_close($curl);
+            $dataSending = array();
+            $dataSending["api_key"] = "OG0UMAWHV6SJ0GL2";
+            $dataSending["number_key"] = "liVWjKEiV6aImQ5M";
+            $dataSending["phone_no"] = $phone;
+            $dataSending["message"] = "Halo " . $name . ", ini adalah trial WA";
 
-        $resArray = json_decode($response, true);
+            $curl = curl_init();
+            curl_setopt_array($curl, array(
+                CURLOPT_URL => 'https://api.watzap.id/v1/send_message',
+                CURLOPT_RETURNTRANSFER => true,
+                CURLOPT_ENCODING => '',
+                CURLOPT_MAXREDIRS => 10,
+                CURLOPT_TIMEOUT => 0,
+                CURLOPT_FOLLOWLOCATION => true,
+                CURLOPT_HTTP_VERSION => CURL_HTTP_VERSION_1_1,
+                CURLOPT_CUSTOMREQUEST => 'POST',
+                CURLOPT_POSTFIELDS => json_encode($dataSending),
+                CURLOPT_HTTPHEADER => array(
+                    'Content-Type: application/json'
+                ),
+            ));
+            $response = curl_exec($curl);
+            curl_close($curl);
 
-        if ($resArray['status'] == 200) {
-            $arrayWatzapTukang = [
-                'status_watzap_tukang' => 'sent',
-                'updated_at' => date('Y-m-d H:i:s')
-            ];
+            $resArray = json_decode($response, true);
 
-            $updateStatus = $this->MWatzaptukang->updateFromArray($watzapTukang['id_watzap_tukang'], $arrayWatzapTukang);
-
-            if ($updateStatus) {
-                $dataResponse = [
-                    'status' => 'ok',
-                    'msg' => 'Succes sent and upadate status'
+            if ($resArray['status'] == 200) {
+                $arrayWatzapTukang = [
+                    'status_watzap_tukang' => 'sent',
+                    'updated_at' => date('Y-m-d H:i:s')
                 ];
 
-                $this->output->set_output(json_encode($dataResponse));
+                $updateStatus = $this->MWatzaptukang->updateFromArray($watzapTukang['id_watzap_tukang'], $arrayWatzapTukang);
+
+                if ($updateStatus) {
+                    $dataResponse = [
+                        'status' => 'ok',
+                        'msg' => 'Succes sent and upadate status'
+                    ];
+
+                    $this->output->set_output(json_encode($dataResponse));
+                } else {
+                    $dataResponse = [
+                        'status' => 'failed',
+                        'msg' => 'Failed to update status'
+                    ];
+
+                    $this->output->set_output(json_encode($dataResponse));
+                }
             } else {
                 $dataResponse = [
                     'status' => 'failed',
-                    'msg' => 'Failed to update status'
+                    'response' => $resArray
                 ];
 
                 $this->output->set_output(json_encode($dataResponse));
@@ -69,7 +79,7 @@ class Watzap extends CI_Controller
         } else {
             $dataResponse = [
                 'status' => 'failed',
-                'response' => $resArray
+                'msg' => 'No numbers to send'
             ];
 
             $this->output->set_output(json_encode($dataResponse));
