6103b1a4a94f7cd0146325a258b05817e1bbf430
Al Ghifari Anwar <alghifari.anwar2002@gmail.com>
2025-02-19 14:52:12 +0700

    rekap stok

--- application/views/Theme/Menu.php
+++ application/views/Theme/Menu.php
@@ -118,7 +118,7 @@
                                     </p>
                                 </a>
                             </li>
-                            <?php if ($this->session->userdata('level_user') == 'admin' || $this->session->userdata('level_user') == 'salesleader' || $this->session->userdata('level_user') == 'admin_c'): ?>
+                            <?php if ($this->session->userdata('level_user') == 'admin' || $this->session->userdata('level_user') == 'salesleader' || $this->session->userdata('level_user') == 'admin_c' || $this->session->userdata('level_user') == 'finance'): ?>
                                 <li class="nav-item">
                                     <a href="#" class="nav-link">
                                         <i class="nav-icon fas fa-cog"></i>
