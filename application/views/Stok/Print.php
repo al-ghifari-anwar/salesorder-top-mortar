3e45f8a440b2fe0c94a297c5ef38c1c8e4d1df40
Al Ghifari Anwar <alghifari.anwar2002@gmail.com>
2025-02-23 23:22:47 +0700

    Fix stok out

--- application/views/Stok/Print.php
+++ application/views/Stok/Print.php
@@ -136,7 +136,7 @@ function penyebut($nilai)
                 $this->db->where_in('id_city', "SELECT id_city FROM tb_city tc WHERE id_gudang_stok = '$id_gudang_stok'");
                 $getProdukDatas = $this->db->get('tb_produk')->result_array();
 
-                echo json_encode($getProdukDatas);
+                echo $this->db->last_query();
                 die;
 
                 $idProduks = array();
