791c05ebc647bc409c7445667e1229e4587b6ce6
Al Ghifari Anwar <alghifari.anwar2002@gmail.com>
2025-02-23 20:52:10 +0700

    Remove Piutang Zero

--- application/views/Piutang/Print.php
+++ application/views/Piutang/Print.php
@@ -131,7 +131,7 @@ function penyebut($nilai)
                 <?php if ($notZeroInv > 0): ?>
                     <tr>
                         <th><?= $no++ ?></th>
-                        <th class="text-left"><?= $dataInv['nama'] . " - " . $dataInv['kode_city'] ?></th>
+                        <th class="text-left"><?= $notZeroInv ?><?= $dataInv['nama'] . " - " . $dataInv['kode_city'] ?></th>
                         <td colspan="5"></td>
                     </tr>
                     <?php
