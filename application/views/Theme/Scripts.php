ba39a4ff66a32ff38310e0aee8ec3e15d9d16f9b
Al Ghifari Anwar <alghifari.anwar2002@gmail.com>
2025-03-11 01:49:52 +0700

    Data tukang posted by

--- application/views/Theme/Scripts.php
+++ application/views/Theme/Scripts.php
@@ -47,6 +47,10 @@
     });
 
     $("#table-print").DataTable({
+        "lengthMenu": [
+            [10, 25, 50, 100, -1],
+            [10, 25, 50, 100, "All"]
+        ],
         "paging": true,
         "responsive": true,
         "lengthChange": false,
