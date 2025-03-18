758d591ec126a120f530f4ef795198abec23a014
Al Ghifari Anwar <alghifari.anwar2002@gmail.com>
2025-03-18 09:45:54 +0700

    konten tukang

--- application/controllers/Watzap.php
+++ application/controllers/Watzap.php
@@ -114,7 +114,7 @@ class Watzap extends CI_Controller
 
             // $resArray = json_decode($response, true);
 
-            if ($resArray['status'] == 200) {
+            if ($resArray['content'] == "Message sent") {
                 $arrayWatzapTukang = [
                     'send_at' => date('Y-m-d H:i:s'),
                     'status_watzap_tukang' => 'sent',
