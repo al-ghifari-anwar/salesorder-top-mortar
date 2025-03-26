cfb27b1f9a52cc67fc603c0efedfef92d7cbf86b
Al Ghifari Anwar <alghifari.anwar2002@gmail.com>
2025-03-26 12:13:13 +0700

    Scoring tabel

--- application/models/MInvoice.php
+++ application/models/MInvoice.php
@@ -69,8 +69,16 @@ class MInvoice extends CI_Model
     {
         $this->db->join('tb_surat_jalan', 'tb_surat_jalan.id_surat_jalan = tb_invoice.id_surat_jalan');
         $this->db->join('tb_contact', 'tb_contact.id_contact = tb_surat_jalan.id_contact');
-        $this->db->order_by('tb_surat_jalan.id_contact', 'ASC');
         $query = $this->db->get_where('tb_invoice', ['tb_surat_jalan.id_contact' => $id_contact, 'tb_invoice.total_invoice >' => 1000, 'tb_invoice.status_invoice' => 'paid'])->result_array();
+        return $query;
+    }
+
+    public function getLast3PaidByIdContactNoMerch($id_contact)
+    {
+        $this->db->join('tb_surat_jalan', 'tb_surat_jalan.id_surat_jalan = tb_invoice.id_surat_jalan');
+        $this->db->join('tb_contact', 'tb_contact.id_contact = tb_surat_jalan.id_contact');
+        $this->db->order_by('tb_invoice.date_invoice', 'DESC');
+        $query = $this->db->get_where('tb_invoice', ['tb_surat_jalan.id_contact' => $id_contact, 'tb_invoice.total_invoice >' => 1000, 'tb_invoice.status_invoice' => 'paid'], 3)->result_array();
         return $query;
     }
 
