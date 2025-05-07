fe05037697ff14a52d98737a89754070e77ab0a1
Al Ghifari Anwar <alghifari.anwar2002@gmail.com>
2025-05-07 16:07:42 +0700

    Test

--- application/views/Invoice/List.php
+++ application/views/Invoice/List.php
@@ -49,6 +49,8 @@
                                         <th>No</th>
                                         <th>No Invoice</th>
                                         <th>No DO</th>
+                                        <th>Termin</th>
+                                        <th>Jatem</th>
                                         <th>Total</th>
                                         <th>Aksi</th>
                                     </tr>
@@ -57,13 +59,30 @@
                                     <?php
                                     $no = 1;
                                     foreach ($invoice as $data) : ?>
+                                        <?php
+                                        if ($data['is_cod'] == 0) {
+                                            // Set Jatem
+                                            $date_jatem = date('Y-m-d H:i:s', strtotime("+" . $rowRenvis['termin_payment'] . " days", strtotime($rowRenvis['date_invoice'])));
+                                            // Count Days
+                                            $jatuhTempo = date('d M Y', strtotime("+" . $rowRenvis['termin_payment'] . " days", strtotime($rowRenvis['date_invoice'])));
+                                            $date1 = new DateTime(date("Y-m-d"));
+                                            $date2 = new DateTime($jatuhTempo);
+                                            $days  = $date2->diff($date1)->format('%a');
+                                        } else {
+                                            $date_jatem = date("Y-m-d H:i:s", strtotime($data['date_invoice']));
+                                            $days = "COD";
+                                        }
+                                        ?>
                                         <tr>
                                             <td><?= $no++; ?></td>
                                             <td><?= $data['no_invoice'] ?></td>
                                             <td><?= $data['no_surat_jalan'] ?></td>
+                                            <td><?= $days ?></td>
+                                            <td><?= $date_jatem ?></td>
                                             <td>Rp. <?= number_format($data['total_invoice'], 0, ',', '.') ?></td>
                                             <td>
                                                 <a href="<?= base_url('print-invoice/' . $data['id_invoice']) ?>" class="btn btn-success" title="Hapus" target="__blank"><i class="fas fa-print"></i></a>
+                                                <a href="<?= base_url('invoice/change' . $data['id_invoice']) ?>" class="btn btn-success" title="Ubah" target="__blank"><i class="fas fa-print"></i></a>
                                             </td>
                                         </tr>
 
