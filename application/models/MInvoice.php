b4b93ed6ab403f299a4a7a3d3e2bc825ba4769da
Al Ghifari Anwar <alghifari.anwar2002@gmail.com>
2025-05-17 15:17:09 +0700

    Scoring combine

--- application/models/MInvoice.php
+++ application/models/MInvoice.php
@@ -263,7 +263,7 @@ class MInvoice extends CI_Model
         $this->db->join('tb_contact', 'tb_contact.id_contact = tb_surat_jalan.id_contact');
         $this->db->join('tb_city', 'tb_contact.id_city = tb_city.id_city');
         $this->db->order_by('tb_invoice.date_invoice', 'ASC');
-        $query = $this->db->get_where('tb_invoice', ['tb_contact.id_contact' => $id_contact, "DATE_FORMAT(date_invoice, '%Y-%m')" => $month], 1)->row_array();
+        $query = $this->db->get_where('tb_invoice', ['tb_contact.id_contact' => $id_contact, "DATE_FORMAT(date_invoice, '%Y-%m') =" => $month], 1)->row_array();
         return $query;
     }
 
