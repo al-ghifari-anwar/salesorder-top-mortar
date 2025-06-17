581fd19007c0123fcd275899004f83fe6f5492d5
Al Ghifari Anwar <alghifari.anwar2002@gmail.com>
2025-06-17 10:54:51 +0700

    status contact voucher

--- application/models/MContact.php
+++ application/models/MContact.php
@@ -104,6 +104,7 @@ class MContact extends CI_Model
     public function getAllForVouchers($id_city)
     {
         $this->db->join('tb_city', 'tb_city.id_city = tb_contact.id_city');
+        $this->db->where('tb_contact.store_status', 'passive');
         $query = $this->db->get_where('tb_contact', ['tb_contact.id_city' => $id_city])->result_array();
         return $query;
     }
