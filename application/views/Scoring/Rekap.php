ee76dae7aa061814250d5a15c92a16c7420b2059
Al Ghifari Anwar <alghifari.anwar2002@gmail.com>
2025-06-23 13:35:25 +0700

    3 Inv terakhir waktu jatem saja

--- application/views/Scoring/Rekap.php
+++ application/views/Scoring/Rekap.php
@@ -272,52 +272,15 @@ function penyebut($nilai)
 
                 $jatuhTempo = date('Y-m-d', strtotime("+" . $contact['termin_payment'] . " days", strtotime($last_invoice['date_invoice'])));
 
-                if ($jatuhTempo <= date("Y-m-d")) {
+                // if ($jatuhTempo <= date("Y-m-d")) {
 
-                    if ($payments) {
+                if ($payments) {
 
-                        foreach ($payments as $payment) {
-                            $datePayment = date("Y-m-d", strtotime($payment['date_payment']));
-                            if ($datePayment > $jatuhTempo) {
-                                // $count_late_payment += 1;
-                                $date1 = new DateTime($datePayment);
-                                $date2 = new DateTime($jatuhTempo);
-                                $days  = $date2->diff($date1)->format('%a');
-
-                                $scoreData = [
-                                    'id_invoice' => $last_invoice['id_invoice'],
-                                    'no_invoice' => $last_invoice['no_invoice'],
-                                    'status' => 'late',
-                                    'days_late' => $days,
-                                    'date_jatem' => $jatuhTempo,
-                                    'date_payment' => $datePayment,
-                                    'percent_score' => 100 - $days,
-                                    'is_cod' => $sj['is_cod'],
-                                    'date_invoice' => $last_invoice['date_invoice'],
-                                ];
-
-                                array_push($last_array_scoring, $scoreData);
-                            } else {
-                                $scoreData = [
-                                    'id_invoice' => $last_invoice['id_invoice'],
-                                    'no_invoice' => $last_invoice['no_invoice'],
-                                    'status' => 'good',
-                                    'days_late' => 0,
-                                    'date_jatem' => $jatuhTempo,
-                                    'date_payment' => $datePayment,
-                                    'percent_score' => 100,
-                                    'is_cod' => $sj['is_cod'],
-                                    'date_invoice' => $last_invoice['date_invoice'],
-                                ];
-
-                                array_push($last_array_scoring, $scoreData);
-                            }
-                        }
-                    } else {
-                        $dateNow = date("Y-m-d");
-                        if ($dateNow > $jatuhTempo) {
+                    foreach ($payments as $payment) {
+                        $datePayment = date("Y-m-d", strtotime($payment['date_payment']));
+                        if ($datePayment > $jatuhTempo) {
                             // $count_late_payment += 1;
-                            $date1 = new DateTime($dateNow);
+                            $date1 = new DateTime($datePayment);
                             $date2 = new DateTime($jatuhTempo);
                             $days  = $date2->diff($date1)->format('%a');
 
@@ -327,7 +290,7 @@ function penyebut($nilai)
                                 'status' => 'late',
                                 'days_late' => $days,
                                 'date_jatem' => $jatuhTempo,
-                                'date_payment' => $dateNow,
+                                'date_payment' => $datePayment,
                                 'percent_score' => 100 - $days,
                                 'is_cod' => $sj['is_cod'],
                                 'date_invoice' => $last_invoice['date_invoice'],
@@ -341,7 +304,7 @@ function penyebut($nilai)
                                 'status' => 'good',
                                 'days_late' => 0,
                                 'date_jatem' => $jatuhTempo,
-                                'date_payment' => $dateNow,
+                                'date_payment' => $datePayment,
                                 'percent_score' => 100,
                                 'is_cod' => $sj['is_cod'],
                                 'date_invoice' => $last_invoice['date_invoice'],
@@ -350,7 +313,44 @@ function penyebut($nilai)
                             array_push($last_array_scoring, $scoreData);
                         }
                     }
+                } else {
+                    $dateNow = date("Y-m-d");
+                    if ($dateNow > $jatuhTempo) {
+                        // $count_late_payment += 1;
+                        $date1 = new DateTime($dateNow);
+                        $date2 = new DateTime($jatuhTempo);
+                        $days  = $date2->diff($date1)->format('%a');
+
+                        $scoreData = [
+                            'id_invoice' => $last_invoice['id_invoice'],
+                            'no_invoice' => $last_invoice['no_invoice'],
+                            'status' => 'late',
+                            'days_late' => $days,
+                            'date_jatem' => $jatuhTempo,
+                            'date_payment' => $dateNow,
+                            'percent_score' => 100 - $days,
+                            'is_cod' => $sj['is_cod'],
+                            'date_invoice' => $last_invoice['date_invoice'],
+                        ];
+
+                        array_push($last_array_scoring, $scoreData);
+                    } else {
+                        $scoreData = [
+                            'id_invoice' => $last_invoice['id_invoice'],
+                            'no_invoice' => $last_invoice['no_invoice'],
+                            'status' => 'good',
+                            'days_late' => 0,
+                            'date_jatem' => $jatuhTempo,
+                            'date_payment' => $dateNow,
+                            'percent_score' => 100,
+                            'is_cod' => $sj['is_cod'],
+                            'date_invoice' => $last_invoice['date_invoice'],
+                        ];
+
+                        array_push($last_array_scoring, $scoreData);
+                    }
                 }
+                // }
             }
 
             // Scoring System
