37df6a54bd9549e42dbd7635469a56a64ae27375
Al Ghifari Anwar <alghifari.anwar2002@gmail.com>
2025-07-17 01:44:06 +0700

    Invoice COD belum lunas

--- application/models/MInvoice.php
+++ application/models/MInvoice.php
@@ -56,7 +56,7 @@ class MInvoice extends CI_Model
         $this->db->join('tb_surat_jalan', 'tb_surat_jalan.id_surat_jalan = tb_invoice.id_surat_jalan');
         $this->db->join('tb_contact', 'tb_contact.id_contact = tb_surat_jalan.id_contact');
         $this->db->order_by('id_invoice', 'DESC');
-        $query = $this->db->get_where('tb_invoice', ['tb_contact.id_city' => $id_city, 'tb_surat_jalan.is_cod' => 1, 'tb_invoice.status_invoice' => 'Waiting'])->result_array();
+        $query = $this->db->get_where('tb_invoice', ['tb_contact.id_city' => $id_city, 'tb_surat_jalan.is_cod' => 1, 'tb_invoice.status_invoice' => 'waiting'])->result_array();
         return $query;
     }
 
