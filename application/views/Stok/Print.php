29a526752783ab9010c450a7083f491eaacc20da
Al Ghifari Anwar <alghifari.anwar2002@gmail.com>
2025-07-20 10:34:35 +0700

    Reset stok opname

--- application/views/Stok/Print.php
+++ application/views/Stok/Print.php
@@ -191,8 +191,8 @@ function penyebut($nilai)
 
                 $jmlAwal = $jumlahAwal['jml_stokAwal'] != null ? $jumlahAwal['jml_stokAwal'] : 0;
                 $jmlKluarAwal = $jumlahAwalPengeluaran['jml_stokOut'] != null ? $jumlahAwalPengeluaran['jml_stokOut'] : 0;
-                $valPemasukan = $getStokIn['jml_stokIn'];
-                $valPengeluaran = $getStokOut == null ? $getStokOut['jml_stokOut'] : 0;
+                $valPemasukan = $getStokIn != null ? $getStokIn['jml_stokIn'] : 0;
+                $valPengeluaran = $getStokOut != null ? $getStokOut['jml_stokOut'] : 0;
                 $valJumlahAwal = $jmlAwal - $jmlKluarAwal;
                 $valJumlahAkhir = ($valJumlahAwal + $getStokIn['jml_stokIn']) - $getStokOut['jml_stokOut'];
                 ?>
