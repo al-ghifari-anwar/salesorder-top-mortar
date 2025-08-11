09a077a1b7764420e677ceb55165cf6ec371cb5b
Al Ghifari Anwar <alghifari.anwar2002@gmail.com>
2025-08-11 18:45:44 +0700

    SJ bug

--- application/views/SuratJalan/Detail.php
+++ application/views/SuratJalan/Detail.php
@@ -67,7 +67,8 @@
                                 <div class="col-1">
                                     <?php if ($detail == null) { ?>
                                         <a href="<?= base_url('finish-suratjalan/') . $suratjalan['id_surat_jalan'] ?>" class="btn btn-secondary disabled">Finish</a>
-                                    <?php } else if ($detail != null && $suratjalan['is_finished'] == 0) { ?>
+                                    <?php } else if ($detail != null) { ?>
+                                        <?php if ($suratjalan['is_finished'] == 0 || $suratjalan['is_closing'] == 0) ?>
                                         <a href="<?= base_url('finish-suratjalan/') . $suratjalan['id_surat_jalan'] ?>" class="btn btn-success">Finish</a>
                                     <?php } ?>
 
