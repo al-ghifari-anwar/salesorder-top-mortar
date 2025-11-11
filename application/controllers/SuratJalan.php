176bfbe5b4bd96bd067efafd1f8392d56e1ba46d
Al Ghifari Anwar <alghifari.anwar2002@gmail.com>
2025-11-11 14:17:12 +0700

    Fix bypass closing

--- application/controllers/SuratJalan.php
+++ application/controllers/SuratJalan.php
@@ -156,6 +156,25 @@ class SuratJalan extends CI_Controller
                     $res = json_decode($response, true);
 
                     if ($res['response'] == 200) {
+                        // Save delivery
+                        $curl = curl_init();
+
+                        curl_setopt_array($curl, array(
+                            CURLOPT_URL => 'https://devsaleswa.topmortarindonesia.com//delivery.php',
+                            CURLOPT_RETURNTRANSFER => true,
+                            CURLOPT_ENCODING => '',
+                            CURLOPT_MAXREDIRS => 10,
+                            CURLOPT_TIMEOUT => 0,
+                            CURLOPT_FOLLOWLOCATION => true,
+                            CURLOPT_HTTP_VERSION => CURL_HTTP_VERSION_1_1,
+                            CURLOPT_CUSTOMREQUEST => 'POST',
+                            CURLOPT_POSTFIELDS => array('endDateTime' => date('Y-m-d H:i:s'), 'endLat' => ' ', 'endLng' => ' ', 'lat' => ' ', 'lng' => ' ', 'id_courier' => $suratJalan['id_courier'], 'id_contact' => $suratJalan['id_contact'], 'startDateTime' => date('Y-m-d ') . '08:00:00', 'startLat' => ' ', 'startLng' => ' '),
+                        ));
+
+                        $response = curl_exec($curl);
+
+                        curl_close($curl);
+
                         $this->session->set_flashdata('success', "Berhasil closing");
                         redirect('suratjalan/' . $suratJalan['id_city']);
                     } else {
