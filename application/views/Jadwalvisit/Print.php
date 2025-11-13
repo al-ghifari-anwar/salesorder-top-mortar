364c3082d0367b85fefb18cd25430c8b56d1d014
Al Ghifari Anwar <alghifari.anwar2002@gmail.com>
2025-11-13 12:16:56 +0700

    Test cluster 1 batch 2

--- application/views/Jadwalvisit/Print.php
+++ application/views/Jadwalvisit/Print.php
@@ -579,7 +579,7 @@ function penyebut($nilai)
             if (count($jadwalVisits) <= 10) {
                 $id_contact = $janjiBayar['id_contact'];
 
-                $rowLastVisit = $this->db->query("SELECT * FROM tb_visit WHERE id_contact = '$id_contact' AND source_visit IN ('voucher','passive','renvisales','mg','normal','jatem1','jatem2','jatem3') ORDER BY date_visit DESC LIMIT 1")->row_array();
+                $rowLastVisit = $this->db->query("SELECT * FROM tb_visit WHERE id_contact = '$id_contact' AND source_visit IN ('voucher','passive','renvisales','mg','normal','jatem1','jatem2','jatem3','weekly') ORDER BY date_visit DESC LIMIT 1")->row_array();
 
                 $date_last_for_counter = date('Y-m-d', strtotime($rowLastVisit['date_visit']));
                 $last_visit = date('d M Y', strtotime($rowLastVisit['date_visit']));
@@ -622,8 +622,10 @@ function penyebut($nilai)
                     'is_new' => 0,
                 ];
 
-                if (array_search($id_contact, array_column($jadwalVisits, 'id_contact')) == "") {
-                    array_push($jadwalVisits, $renvisFilter);
+                if ($days > 3) {
+                    if (array_search($id_contact, array_column($jadwalVisits, 'id_contact')) == "") {
+                        array_push($jadwalVisits, $renvisFilter);
+                    }
                 }
             }
         }
