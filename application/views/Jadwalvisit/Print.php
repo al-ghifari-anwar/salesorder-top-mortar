81170be06a75c4f871cc376352a29b7daa9ba7e3
Al Ghifari Anwar <alghifari.anwar2002@gmail.com>
2025-11-13 09:56:06 +0700

    Test cluster 1 batch 2

--- application/views/Jadwalvisit/Print.php
+++ application/views/Jadwalvisit/Print.php
@@ -579,8 +579,10 @@ function penyebut($nilai)
             if (count($jadwalVisits) <= 10) {
                 $id_contact = $janjiBayar['id_contact'];
 
-                $date_last_for_counter = date('Y-m-d', strtotime($janjiBayar['date_visit']));
-                $last_visit = date('d M Y', strtotime($janjiBayar['date_visit']));
+                $rowLastVisit = $this->db->query("SELECT * FROM tb_visit WHERE id_contact = '$id_contact' AND source_visit IN ('voucher','passive','renvisales','mg','normal','jatem1','jatem2','jatem3') ORDER BY date_visit DESC LIMIT 1")->row_array();
+
+                $date_last_for_counter = date('Y-m-d', strtotime($rowLastVisit['date_visit']));
+                $last_visit = date('d M Y', strtotime($rowLastVisit['date_visit']));
 
                 $date1 = new DateTime(date("Y-m-d"));
                 $date2 = new DateTime($date_last_for_counter);
