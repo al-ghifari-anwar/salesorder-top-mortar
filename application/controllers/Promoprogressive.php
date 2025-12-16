6829104b3bf897c51f99b87ba7734a344d42d328
Al Ghifari Anwar <alghifari.anwar2002@gmail.com>
2025-12-16 15:20:03 +0700

    promo progressive

--- application/controllers/Promoprogressive.php
+++ application/controllers/Promoprogressive.php
@@ -19,8 +19,8 @@ class Promoprogressive extends CI_Controller
 
         $this->db->where('deleted_at IS NULL', null, false);
         $data['promos'] = $this->db->get_where('tb_promo_progressive', ['id_distributor' => $this->session->userdata('id_distributor')])->result_array();
-        echo $this->db->last_query();
-        die;
+        // echo $this->db->last_query();
+        // die;
 
         $this->load->view('Theme/Header', $data);
         $this->load->view('Theme/Menu');
