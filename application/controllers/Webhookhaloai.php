94ded751f22be2b92e97e1f19c6220db68de2f67
Al Ghifari Anwar <alghifari.anwar2002@gmail.com>
2026-01-03 09:37:12 +0700

    webhook chat

---
+++ application/controllers/Webhookhaloai.php
@@ -0,0 +1,34 @@
+<?php
+
+class Webhookhaloai extends CI_Controller
+{
+    public function save()
+    {
+        $post = json_decode(file_get_contents('php://input'), true) != null ? json_decode(file_get_contents('php://input'), true) : $this->input->post();
+
+        $webhookData = [
+            'type_webhook_haloai' => 'chat',
+            'content_webhook_haloai' => json_encode($post),
+        ];
+
+        $save = $this->db->insert('tb_webhook_haloai', $webhookData);
+
+        if ($save) {
+            $result = [
+                'code' => 200,
+                'status' => 'ok',
+                'msg' => 'Success',
+            ];
+
+            return $this->output->set_output(json_encode($result));
+        } else {
+            $result = [
+                'code' => 400,
+                'status' => 'failed',
+                'msg' => 'Failed',
+            ];
+
+            return $this->output->set_output(json_encode($result));
+        }
+    }
+}
