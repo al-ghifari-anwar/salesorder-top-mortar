6e5b76fade9755862442ef17d79f15d5bd87f651
Al Ghifari Anwar <alghifari.anwar2002@gmail.com>
2026-01-05 09:59:10 +0700

    AI butuh bantuan

--- application/controllers/SuratJalan.php
+++ application/controllers/SuratJalan.php
@@ -299,7 +299,7 @@ class SuratJalan extends CI_Controller
 
         $resFirstNumber = json_decode($responseFirstNumber, true);
 
-        if (date('Y-m-d', strtotime($resFirstNumber['data']['lastMessageAt'])) >= date('Y-m-d', strtotime("-1 Days"))) {
+        if (date('Y-m-d', strtotime($resFirstNumber['data']['lastMessageAt'])) >= date('Y-m-d', strtotime("-2 Days"))) {
             $this->form_validation->set_rules('order_number', 'Order Number', 'required');
 
             if ($this->form_validation->run() == false) {
@@ -334,7 +334,7 @@ class SuratJalan extends CI_Controller
 
             $resSecondNumber = json_decode($responseSecondNumber, true);
 
-            if (date('Y-m-d', strtotime($resSecondNumber['data']['lastMessageAt'])) >= date('Y-m-d', strtotime("-1 Days"))) {
+            if (date('Y-m-d', strtotime($resSecondNumber['data']['lastMessageAt'])) >= date('Y-m-d', strtotime("-2 Days"))) {
                 $this->form_validation->set_rules('order_number', 'Order Number', 'required');
 
                 if ($this->form_validation->run() == false) {
@@ -344,7 +344,7 @@ class SuratJalan extends CI_Controller
                     $this->MSuratJalan->insert();
                 }
             } else {
-                $this->session->set_flashdata('failed', "Tidak dapat membuat surat jalan, karena toko belum openchat pada hari ini! Openchat terakhir pada: " . date('Y-m-d', strtotime($resFirstNumber['data']['lastMessageAt'])) . ' | ' . date('Y-m-d', strtotime("-1 Days")));
+                $this->session->set_flashdata('failed', "Tidak dapat membuat surat jalan, karena toko belum openchat pada hari ini! Openchat terakhir pada: " . date('Y-m-d', strtotime($resFirstNumber['data']['lastMessageAt'])) . ' | ' . date('Y-m-d', strtotime("-2 Days")));
                 redirect('surat-jalan');
             }
         }
