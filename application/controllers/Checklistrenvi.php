f1e9cefcc260d0ba22e020f09cd4c159678610d8
Al Ghifari Anwar <alghifari.anwar2002@gmail.com>
2026-01-12 07:16:44 +0700

    Notif Tagihan

--- application/controllers/Checklistrenvi.php
+++ application/controllers/Checklistrenvi.php
@@ -7,6 +7,7 @@ class Checklistrenvi extends CI_Controller
         parent::__construct();
         $this->load->model('MRenvi');
         $this->load->model('MInvoice');
+        $this->load->model('HTelegram');
 
         if ($this->session->userdata('id_user') == null) {
             redirect('login');
@@ -376,5 +377,250 @@ class Checklistrenvi extends CI_Controller
             $this->session->set_flashdata('failed', "Gagal mengirim data! . " . json_encode($res));
             redirect('checklistrenvi');
         }
+    }
+
+    public function notif()
+    {
+        // $selectedIds = $this->input->post('ids');
+        // if (empty($selectedIds)) {
+        //     echo "no_data";
+        //     return;
+        // }
+        $citys = $this->db->where('is_notif_jadwal', 1)->get('tb_city')->result_array();
+
+        $resposneTele = array();
+
+        foreach ($citys as $city) {
+
+            $id_city = $city['id_city'];
+
+            $jatem1s = $this->MRenvi->getJatem1($id_city);
+            $jatem2s = $this->MRenvi->getJatem2($id_city);
+            $jatem3s = $this->MRenvi->getJatem3($id_city);
+            $mingguans = $this->MRenvi->getMingguan($id_city);
+
+            $renvis = array();
+
+            foreach ($jatem1s as $jatem1) {
+                $jatem1['selected'] = 'yes';
+                $id_inv = $jatem1['id_invoice'];
+                $count = $this->db->query("SELECT COUNT(*) AS jmlRenvis FROM tb_renvis_jatem WHERE id_invoice = '$id_inv' AND type_renvis = 'jatem1'")->row_array();
+                $jatuhTempo = date('d M Y', strtotime("+" . $jatem1['termin_payment'] . " days", strtotime($jatem1['date_invoice'])));
+                $jatem = date('Y-m-d', strtotime("+" . $jatem1['termin_payment'] . " days", strtotime($jatem1['date_invoice'])));
+                $date1 = new DateTime(date("Y-m-d"));
+                $date2 = new DateTime($jatuhTempo);
+                $days  = $date2->diff($date1)->format('%a');
+                $operan = "";
+                if ($date1 < $date2) {
+                    $operan = "-";
+                }
+                $days = $operan . $days;
+                $jatem1['days'] = $days;
+                $jatem1['jatuh_tempo'] = $jatuhTempo;
+                $jatem1['jatem'] = $jatem;
+                $jatem1['is_new'] = $count['jmlRenvis'] == 1 ? "1" : "0";
+                $id_con = $jatem1['id_contact'];
+                $dateJatem = date('Y-m-d', strtotime("+" . $jatem1['termin_payment'] . " days", strtotime($jatem1['date_invoice'])));
+                $lastVisit = $this->db->query("SELECT * FROM tb_visit WHERE id_contact = '$id_con' AND DATE(date_visit) >= '$dateJatem' AND source_visit IN ('jatem1','jatem2','jatem3','weekly') ORDER BY date_visit DESC LIMIT 1")->row_array();
+                $created_at = $jatem1['created_at'];
+                $jatem1['created_at'] = $lastVisit == null ? $created_at : $lastVisit['date_visit'];
+                $renvis[] = $jatem1;
+            }
+
+            foreach ($jatem2s as $jatem2) {
+                $jatem2['selected'] = 'yes';
+                $id_inv = $jatem2['id_invoice'];
+                $count = $this->db->query("SELECT COUNT(*) AS jmlRenvis FROM tb_renvis_jatem WHERE id_invoice = '$id_inv' AND type_renvis = 'jatem2'")->row_array();
+                $jatuhTempo = date('d M Y', strtotime("+" . $jatem2['termin_payment'] . " days", strtotime($jatem2['date_invoice'])));
+                $jatem = date('Y-m-d', strtotime("+" . $jatem2['termin_payment'] . " days", strtotime($jatem2['date_invoice'])));
+                $date1 = new DateTime(date("Y-m-d"));
+                $date2 = new DateTime($jatuhTempo);
+                $days  = $date2->diff($date1)->format('%a');
+                $operan = "";
+                if ($date1 < $date2) {
+                    $operan = "-";
+                }
+                $days = $operan . $days;
+                $jatem2['days'] = $days;
+                $jatem2['jatuh_tempo'] = $jatuhTempo;
+                $jatem2['jatem'] = $jatem;
+                $jatem2['is_new'] = $count['jmlRenvis'] == 1 ? "1" : "0";
+                $id_con = $jatem2['id_contact'];
+                $dateJatem = date('Y-m-d', strtotime("+" . $jatem2['termin_payment'] . " days", strtotime($jatem2['date_invoice'])));
+                $lastVisit = $this->db->query("SELECT * FROM tb_visit WHERE id_contact = '$id_con' AND DATE(date_visit) >= '$dateJatem' AND source_visit IN ('jatem1','jatem2','jatem3','weekly') ORDER BY date_visit DESC LIMIT 1")->row_array();
+                $created_at = $jatem2['created_at'];
+                $jatem2['created_at'] = $lastVisit == null ? $created_at : $lastVisit['date_visit'];
+                $renvis[] = $jatem2;
+            }
+
+            foreach ($jatem3s as $jatem3) {
+                $jatem3['selected'] = 'yes';
+                $id_inv = $jatem3['id_invoice'];
+                $count = $this->db->query("SELECT COUNT(*) AS jmlRenvis FROM tb_renvis_jatem WHERE id_invoice = '$id_inv' AND type_renvis = 'jatem3'")->row_array();
+                $jatuhTempo = date('d M Y', strtotime("+" . $jatem3['termin_payment'] . " days", strtotime($jatem3['date_invoice'])));
+                $jatem = date('Y-m-d', strtotime("+" . $jatem3['termin_payment'] . " days", strtotime($jatem3['date_invoice'])));
+                $date1 = new DateTime(date("Y-m-d"));
+                $date2 = new DateTime($jatuhTempo);
+                $days  = $date2->diff($date1)->format('%a');
+                $operan = "";
+                if ($date1 < $date2) {
+                    $operan = "-";
+                }
+                $days = $operan . $days;
+                $jatem3['days'] = $days;
+                $jatem3['jatuh_tempo'] = $jatuhTempo;
+                $jatem3['jatem'] = $jatem;
+                $jatem3['is_new'] = $count['jmlRenvis'] == 1 ? "1" : "0";
+                $id_con = $jatem3['id_contact'];
+                $dateJatem = date('Y-m-d', strtotime("+" . $jatem3['termin_payment'] . " days", strtotime($jatem3['date_invoice'])));
+                $lastVisit = $this->db->query("SELECT * FROM tb_visit WHERE id_contact = '$id_con' AND DATE(date_visit) >= '$dateJatem' AND source_visit IN ('jatem1','jatem2','jatem3','weekly') ORDER BY date_visit DESC LIMIT 1")->row_array();
+                $created_at = $jatem3['created_at'];
+                $jatem3['created_at'] = $lastVisit == null ? $created_at : $lastVisit['date_visit'];
+                $renvis[] = $jatem3;
+            }
+
+            foreach ($mingguans as $mingguan) {
+                $id_inv = $mingguan['id_invoice'];
+                $mingguan['id_renvis_jatem'] = $mingguan['id_rencana_visit'];
+                $mingguan['selected'] = 'yes';
+                $count = $this->db->query("SELECT COUNT(*) AS jmlRenvis FROM tb_renvis_jatem WHERE id_invoice = '$id_inv' AND type_renvis = 'jatem3'")->row_array();
+                $jatuhTempo = date('d M Y', strtotime("+" . $mingguan['termin_payment'] . " days", strtotime($mingguan['date_invoice'])));
+                $jatem = date('Y-m-d', strtotime("+" . $mingguan['termin_payment'] . " days", strtotime($mingguan['date_invoice'])));
+                $date1 = new DateTime(date("Y-m-d"));
+                $date2 = new DateTime($jatuhTempo);
+                $days  = $date2->diff($date1)->format('%a');
+                $operan = "";
+                if ($date1 < $date2) {
+                    $operan = "-";
+                }
+                $days = $operan . $days;
+                $mingguan['days'] = $days;
+                $mingguan['jatuh_tempo'] = $jatuhTempo;
+                $mingguan['jatem'] = $jatem;
+                $mingguan['is_new'] = $count['jmlRenvis'] == 1 ? "1" : "0";
+                $id_con = $mingguan['id_contact'];
+                $dateJatem = date('Y-m-d', strtotime("+" . $mingguan['termin_payment'] . " days", strtotime($mingguan['date_invoice'])));
+                $lastVisit = $this->db->query("SELECT * FROM tb_visit WHERE id_contact = '$id_con' AND DATE(date_visit) >= '$dateJatem' AND source_visit IN ('jatem1','jatem2','jatem3','weekly') ORDER BY date_visit DESC LIMIT 1")->row_array();
+                $created_at = $mingguan['created_at'];
+                $mingguan['created_at'] = $lastVisit == null ? $created_at : $lastVisit['date_visit'];
+                $mingguan['type_renvis'] = $mingguan['type_rencana'];
+                $renvis[] = $mingguan;
+            }
+
+            // echo json_encode($renvis);
+            // die;
+
+            $data['renvis'] = $renvis;
+
+            // Buat direktori penyimpanan sementara
+            $folderPath = FCPATH . 'assets/tmp/renvis/';
+            // Nama file berdasarkan invoice ID + timestamp
+            $fileName = 'Tagihan_' . $city['nama_city'] . '_' . date('d-m-Y') . '_' . time() . '.pdf';
+            $filePath = $folderPath . $fileName;
+            $fileUrl = "https://order.topmortarindonesia.com/assets/tmp/renvis/" . $fileName;
+
+            $mpdf = new \Mpdf\Mpdf(['format' => 'A4']);
+            $mpdf->SetMargins(0, 0, 5);
+            $html = $this->load->view('ChecklistRenvi/Print', $data, true);
+            $mpdf->AddPage('P');
+            $mpdf->WriteHTML($html);
+            $mpdf->Output($filePath, \Mpdf\Output\Destination::FILE);
+            // $mpdf->Output();
+
+            // $user = $this->db->get_where('tb_user', ['id_user' => $this->session->userdata('id_user')])->row_array();
+
+            // Send Message
+            // $id_distributor = $user['id_distributor'];
+            // $nomorhp = $user['phone_user'];
+            // $nama = $user['full_name'];
+            // $template_id = "bd507a74-4fdf-4692-8199-eb4ed8864bc7";
+            // $message = "Checklist visit anda";
+            // $full_name = "-";
+
+            // $qontak = $this->db->get_where('tb_qontak', ['id_distributor' => $id_distributor])->row_array();
+
+            // $wa_token = $qontak['token'];
+            // $integration_id = $qontak['integration_id'];
+            $chatId = "-1003589286815";
+            // Send Invoice
+            // $curl = curl_init();
+
+            // curl_setopt_array($curl, array(
+            //     CURLOPT_URL => 'https://service-chat.qontak.com/api/open/v1/broadcasts/whatsapp/direct',
+            //     CURLOPT_RETURNTRANSFER => true,
+            //     CURLOPT_ENCODING => '',
+            //     CURLOPT_MAXREDIRS => 10,
+            //     CURLOPT_TIMEOUT => 0,
+            //     CURLOPT_FOLLOWLOCATION => true,
+            //     CURLOPT_HTTP_VERSION => CURL_HTTP_VERSION_1_1,
+            //     CURLOPT_CUSTOMREQUEST => 'POST',
+            //     CURLOPT_POSTFIELDS => '{
+            //                 "to_number": "' . $nomorhp . '",
+            //                 "to_name": "' . $nama . '",
+            //                 "message_template_id": "' . $template_id . '",
+            //                 "channel_integration_id": "' . $integration_id . '",
+            //                 "language": {
+            //                     "code": "id"
+            //                 },
+            //                 "parameters": {
+            //                     "header":{
+            //                         "format":"DOCUMENT",
+            //                         "params": [
+            //                             {
+            //                                 "key":"url",
+            //                                 "value":"https://order.topmortarindonesia.com/assets/tmp/renvis/' . $fileName . '"
+            //                             },
+            //                             {
+            //                                 "key":"filename",
+            //                                 "value":"' . $fileName . '"
+            //                             }
+            //                         ]
+            //                     },
+            //                     "body": [
+            //                     {
+            //                         "key": "1",
+            //                         "value": "nama",
+            //                         "value_text": "' . $nama . '"
+            //                     },
+            //                     {
+            //                         "key": "2",
+            //                         "value": "message",
+            //                         "value_text": "' . trim(preg_replace('/\s+/', ' ', $message)) . '"
+            //                     },
+            //                     {
+            //                         "key": "3",
+            //                         "value": "sales",
+            //                         "value_text": "' . $full_name . '"
+            //                     }
+            //                     ]
+            //                 }
+            //                 }',
+            //     CURLOPT_HTTPHEADER => array(
+            //         'Authorization: Bearer ' . $wa_token,
+            //         'Content-Type: application/json'
+            //     ),
+            // ));
+
+            // $response = curl_exec($curl);
+
+            // curl_close($curl);
+
+            // $res = json_decode($response, true);
+
+            $message = "*Data Tagihan*\nKota: *" . $city['nama_city'] . "*\nTanggal: *" . date('d F Y') . "*";
+
+            $send = $this->HTelegram->sendDocumentGroup($chatId, $message, $fileUrl);
+
+            array_push($resposneTele, $send);
+        }
+
+        $result = [
+            'code' => 200,
+            'status' => 'ok',
+            'msg' => 'Success',
+            'data' => $resposneTele,
+        ];
+
+        return $this->output->set_output(json_encode($result));
     }
 }
