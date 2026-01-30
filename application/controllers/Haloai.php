310bb7e1a7c21f768d8e999016d3cf4603c77ff7
Al Ghifari Anwar <alghifari.anwar2002@gmail.com>
2026-01-30 11:06:41 +0700

    Promo lain dibawah settingan promo default

--- application/controllers/Haloai.php
+++ application/controllers/Haloai.php
@@ -53,7 +53,7 @@ class Haloai extends CI_Controller
 
             $kelipatan_promo_toko = $promo['kelipatan_promo'];
 
-            $otherPromo = $this->db->select('nama_promo')->get_where('tb_promo', ['kelipatan_promo <' => $kelipatan_promo_toko])->result_array();
+            $otherPromo = $this->db->select('nama_promo')->get_where('tb_promo', ['kelipatan_promo <' => $kelipatan_promo_toko, 'is_potongan' => 1])->result_array();
 
             // Score toko
             $paymentScore = $this->paymentScoring($contact);
