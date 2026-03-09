ce82693f2aaccb4ba9d4433dd11833e834d52ef5
Al Ghifari Anwar <alghifari.anwar2002@gmail.com>
2026-03-09 11:31:16 +0700

    Send notif and voucher after analisa report visit

--- application/controllers/HaloaiReportVisit.php
+++ application/controllers/HaloaiReportVisit.php
@@ -108,13 +108,282 @@ class HaloaiReportVisit extends CI_Controller
             $save = $this->db->insert('tb_ai_reportvisit', $aiReportvisitData);
 
             if ($save) {
-                $result = [
-                    'code' => 200,
-                    'status' => 'success',
-                    'msg' => 'AI Success response saved',
-                ];
+                $giveVoucher = isset($responseData['give_voucher']) ? $responseData['give_voucher'] : null;
 
-                return $this->output->set_output(json_encode($result));
+                if ($giveVoucher != null) {
+                    if ($giveVoucher == 'yes') {
+                        // Kasih voucher
+                        $curl = curl_init();
+                        $id_contact = $contact['id_contact'];
+                        $jml_voucher = 1;
+
+                        curl_setopt_array($curl, array(
+                            CURLOPT_URL => 'https://saleswa.topmortarindonesia.com/insertVoucher.php?j=' . $jml_voucher . '&s=' . $id_contact . '&t=m',
+                            CURLOPT_RETURNTRANSFER => true,
+                            CURLOPT_ENCODING => '',
+                            CURLOPT_MAXREDIRS => 10,
+                            CURLOPT_TIMEOUT => 0,
+                            CURLOPT_FOLLOWLOCATION => true,
+                            CURLOPT_HTTP_VERSION => CURL_HTTP_VERSION_1_1,
+                            CURLOPT_CUSTOMREQUEST => 'GET',
+                        ));
+
+                        $response = curl_exec($curl);
+
+                        curl_close($curl);
+
+                        $res = json_decode($response, true);
+
+                        $statusVc = $res['status'];
+
+                        if ($statusVc == 'ok') {
+                            // Get HaloAI
+                            $id_distributor = $this->session->userdata('id_distributor');
+                            $haloai = $this->db->get_where('tb_haloai', ['id_distributor' => $id_distributor])->row_array();
+                            $wa_token = $haloai['token_haloai'];
+                            $business_id = $haloai['business_id_haloai'];
+                            $channel_id = $haloai['channel_id_haloai'];
+                            $template = 'notif_voucher_1';
+
+                            // Get Vouchers
+                            $dateNow = date("Y-m-d");
+                            $getVoucher = $this->db->query("SELECT * FROM tb_voucher WHERE id_contact = '$id_contact' AND is_claimed = 0 AND date_voucher LIKE '%$dateNow%' ")->result_array();
+                            $vouchers = "";
+                            foreach ($getVoucher as $voucherArr) {
+                                $vouchers .= $voucherArr['no_voucher'] . ",";
+                            }
+
+                            $message = "Hallo " . $contact['nama'] . " Selamat bergabung kembali di keluarga besar Top Mortar! Nikmati layanan 'Pesan Hari Ini, Kirim Hari Ini', dan promo-promo member ekslusif lainnya. Bersama Top Mortar, mari kita maju bersama! Anda mendapatkan " . $jml_voucher . " buah Voucher no seri: " . $vouchers . ".  Tukarkan voucher anda dengan gratis Perekat Bata Ringan sebelum tanggal *" .  date("d M, Y", strtotime("+30 days")) . "* ";
+
+                            $haloaiPayload = [
+                                'activate_ai_after_send' => false,
+                                'channel_id' => $channel_id,
+                                "fallback_template_header" => [
+                                    'filename' => "send_voucher.mp4",
+                                    'type' => 'video',
+                                    'url' => "https://saleswa.topmortarindonesia.com/vids/send_voucher.mp4",
+                                ],
+                                'fallback_template_message' => $template,
+                                'fallback_template_variables' => [
+                                    $contact['nama'],
+                                    trim(preg_replace('/\s+/', ' ', $jml_voucher)),
+                                    $vouchers,
+                                    date("d M, Y", strtotime("+30 days")),
+                                ],
+                                "media" => [
+                                    'filename' => "send_voucher.mp4",
+                                    'type' => 'video',
+                                    'url' => "https://saleswa.topmortarindonesia.com/vids/send_voucher.mp4",
+                                ],
+                                'phone_number' => $contact['nomorhp'],
+                                'text' => trim(preg_replace('/\s+/', ' ', $message)),
+                            ];
+
+                            $curl = curl_init();
+
+                            curl_setopt_array($curl, array(
+                                CURLOPT_URL => 'https://www.haloai.co.id/api/open/channel/whatsapp/v1/sendMessageByPhoneSync',
+                                CURLOPT_RETURNTRANSFER => true,
+                                CURLOPT_ENCODING => '',
+                                CURLOPT_MAXREDIRS => 10,
+                                CURLOPT_TIMEOUT => 0,
+                                CURLOPT_FOLLOWLOCATION => true,
+                                CURLOPT_HTTP_VERSION => CURL_HTTP_VERSION_1_1,
+                                CURLOPT_CUSTOMREQUEST => 'POST',
+                                CURLOPT_POSTFIELDS => json_encode($haloaiPayload),
+                                CURLOPT_HTTPHEADER => array(
+                                    'Authorization: Bearer ' . $wa_token,
+                                    'X-HaloAI-Business-Id: ' . $business_id,
+                                    'Content-Type: application/json'
+                                ),
+                            ));
+
+                            $response = curl_exec($curl);
+
+                            curl_close($curl);
+
+                            $res = json_decode($response, true);
+
+                            $status = $res['status'];
+
+                            if ($status == 'success') {
+                                $result = [
+                                    'code' => 200,
+                                    'status' => 'success',
+                                    'msg' => 'AI Success response saved, voucher yes, notif success',
+                                    'notif' => $res,
+                                ];
+
+                                return $this->output->set_output(json_encode($result));
+                            } else {
+                                $result = [
+                                    'code' => 400,
+                                    'status' => 'failed',
+                                    'msg' => 'AI Success response saved, voucher yes failed, notif failed',
+                                    'notif' => $res,
+                                ];
+
+                                return $this->output->set_output(json_encode($result));
+                            }
+                        } else {
+                            $result = [
+                                'code' => 400,
+                                'status' => 'failed',
+                                'msg' => 'AI Success response saved, voucher failed, notif success',
+                                'notif' => $res,
+                            ];
+
+                            return $this->output->set_output(json_encode($result));
+                        }
+                    } else {
+                        // Get HaloAI
+                        $id_distributor = $contact['id_distributor'];
+                        $haloai = $this->db->get_where('tb_haloai', ['id_distributor' => $id_distributor])->row_array();
+                        $wa_token = $haloai['token_haloai'];
+                        $business_id = $haloai['business_id_haloai'];
+                        $channel_id = $haloai['channel_id_haloai'];
+                        $template = 'info_meeting_baru';
+
+                        $message = $responseData['rekomendasi_wa'];
+
+                        $haloaiPayload = [
+                            'activate_ai_after_send' => false,
+                            'channel_id' => $channel_id,
+                            'fallback_template_message' => $template,
+                            'fallback_template_variables' => [
+                                $contact['nama'],
+                                trim(preg_replace('/\s+/', ' ', $message)),
+                                "PT Top Mortar Indonesia",
+                            ],
+                            'phone_number' => $contact['nomorhp'],
+                            'text' => trim(preg_replace('/\s+/', ' ', $message)),
+                        ];
+
+                        $curl = curl_init();
+
+                        curl_setopt_array($curl, array(
+                            CURLOPT_URL => 'https://www.haloai.co.id/api/open/channel/whatsapp/v1/sendMessageByPhoneSync',
+                            CURLOPT_RETURNTRANSFER => true,
+                            CURLOPT_ENCODING => '',
+                            CURLOPT_MAXREDIRS => 10,
+                            CURLOPT_TIMEOUT => 0,
+                            CURLOPT_FOLLOWLOCATION => true,
+                            CURLOPT_HTTP_VERSION => CURL_HTTP_VERSION_1_1,
+                            CURLOPT_CUSTOMREQUEST => 'POST',
+                            CURLOPT_POSTFIELDS => json_encode($haloaiPayload),
+                            CURLOPT_HTTPHEADER => array(
+                                'Authorization: Bearer ' . $wa_token,
+                                'X-HaloAI-Business-Id: ' . $business_id,
+                                'Content-Type: application/json'
+                            ),
+                        ));
+
+                        $response = curl_exec($curl);
+
+                        curl_close($curl);
+
+                        $res = json_decode($response, true);
+
+                        $status = $res['status'];
+
+                        if ($status == 'success') {
+                            $result = [
+                                'code' => 200,
+                                'status' => 'success',
+                                'msg' => 'AI Success response saved, voucher no, notif success',
+                                'notif' => $res,
+                            ];
+
+                            return $this->output->set_output(json_encode($result));
+                        } else {
+                            $result = [
+                                'code' => 400,
+                                'status' => 'failed',
+                                'msg' => 'AI Success response saved, voucher no, notif failed',
+                                'notif' => $res,
+                            ];
+
+                            return $this->output->set_output(json_encode($result));
+                        }
+                    }
+                } else {
+                    // Get HaloAI
+                    $id_distributor = $contact['id_distributor'];
+                    $haloai = $this->db->get_where('tb_haloai', ['id_distributor' => $id_distributor])->row_array();
+                    $wa_token = $haloai['token_haloai'];
+                    $business_id = $haloai['business_id_haloai'];
+                    $channel_id = $haloai['channel_id_haloai'];
+                    $template = 'info_meeting_baru';
+
+                    $message = $responseData['rekomendasi_wa'];
+
+                    $haloaiPayload = [
+                        'activate_ai_after_send' => false,
+                        'channel_id' => $channel_id,
+                        'fallback_template_message' => $template,
+                        'fallback_template_variables' => [
+                            $contact['nama'],
+                            trim(preg_replace('/\s+/', ' ', $message)),
+                            "PT Top Mortar Indonesia",
+                        ],
+                        'phone_number' => $contact['nomorhp'],
+                        'text' => trim(preg_replace('/\s+/', ' ', $message)),
+                    ];
+
+                    $curl = curl_init();
+
+                    curl_setopt_array($curl, array(
+                        CURLOPT_URL => 'https://www.haloai.co.id/api/open/channel/whatsapp/v1/sendMessageByPhoneSync',
+                        CURLOPT_RETURNTRANSFER => true,
+                        CURLOPT_ENCODING => '',
+                        CURLOPT_MAXREDIRS => 10,
+                        CURLOPT_TIMEOUT => 0,
+                        CURLOPT_FOLLOWLOCATION => true,
+                        CURLOPT_HTTP_VERSION => CURL_HTTP_VERSION_1_1,
+                        CURLOPT_CUSTOMREQUEST => 'POST',
+                        CURLOPT_POSTFIELDS => json_encode($haloaiPayload),
+                        CURLOPT_HTTPHEADER => array(
+                            'Authorization: Bearer ' . $wa_token,
+                            'X-HaloAI-Business-Id: ' . $business_id,
+                            'Content-Type: application/json'
+                        ),
+                    ));
+
+                    $response = curl_exec($curl);
+
+                    curl_close($curl);
+
+                    $res = json_decode($response, true);
+
+                    $status = $res['status'];
+
+                    if ($status == 'success') {
+                        $result = [
+                            'code' => 200,
+                            'status' => 'success',
+                            'msg' => 'AI Success response saved, voucher null, notif success',
+                            'notif' => $res,
+                        ];
+
+                        return $this->output->set_output(json_encode($result));
+                    } else {
+                        $result = [
+                            'code' => 400,
+                            'status' => 'failed',
+                            'msg' => 'AI Success response saved, voucher null, notif failed',
+                            'notif' => $res,
+                        ];
+
+                        return $this->output->set_output(json_encode($result));
+                    }
+                }
+                // $result = [
+                //     'code' => 200,
+                //     'status' => 'success',
+                //     'msg' => 'AI Success response saved',
+                // ];
+
+                // return $this->output->set_output(json_encode($result));
             } else {
                 $result = [
                     'code' => 400,
