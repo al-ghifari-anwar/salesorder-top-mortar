a832d8c75d7f5a3fc9f567c43bac5cc97e53f29c
Al Ghifari Anwar <alghifari.anwar2002@gmail.com>
2026-07-24 19:49:09 +0700

    tebusmurah

--- application/controllers/Tebusmurah.php
+++ application/controllers/Tebusmurah.php
@@ -7,7 +7,7 @@ class Tebusmurah extends CI_Controller
         $data['contactTebusmurahs'] = $this->db->join('tb_city', 'tb_city.id_city = tb_contact.id_city')->where('tb_contact.store_status', 'active')->where('tb_contact.id_contact IN (SELECT id_contact FROM tb_surat_jalan WHERE is_tebus_murah = 1)', null, false)->order_by('tb_city.nama_city', 'ASC')->get('tb_contact')->result_array();
         // $data = 1;
 
-        json_encode($data);
+        echo json_encode($data);
         die;
         // $html = $this->load->view('Notif/PrintPassive', $data);
         // PDF
