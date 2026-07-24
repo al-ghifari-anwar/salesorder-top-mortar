c8d9634d691e585c50327d2ad3acd6b44511050b
Al Ghifari Anwar <alghifari.anwar2002@gmail.com>
2026-07-24 19:46:58 +0700

    Hobi fix notset

---
+++ application/controllers/Tebusmurah.php
@@ -0,0 +1,20 @@
+<?php
+
+class Tebusmurah extends CI_Controller
+{
+    public function index()
+    {
+        $data['contactTebusmurahs'] = $this->db->join('tb_city', 'tb_city.id_city = tb_contact.id_city')->where('id_contact IN (SELECT id_contact ON tb_surat_jalan WHERE is_tebus_murah = 1)', null, false)->where('tb_contact.store_status', 'active')->order_by('tb_city.nama_city', 'ASC')->get('tb_contact')->result_array();
+        // $data = 1;
+
+        json_encode($data);
+        // $html = $this->load->view('Notif/PrintPassive', $data);
+        // PDF
+        $mpdf = new \Mpdf\Mpdf(['format' => 'A4']);
+        $mpdf->SetMargins(0, 0, 5);
+        $html = $this->load->view('Tebusmurah/Print', $data, true);
+        $mpdf->AddPage('P');
+        $mpdf->WriteHTML($html);
+        $mpdf->Output();
+    }
+}
